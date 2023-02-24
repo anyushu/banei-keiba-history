ba8aadfcde40cfbb7e53fa7d78151b927dab2315
anyushu <anyushu0526@gmail.com>
2023-02-24 10:48:56 +0900

    feat: リッキーくんアニメーション SP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,16 @@ import { useEffect, useState } from 'react'
 import Loading from '@/components/loading'
 
 const Home: NextPage = () => {
+  // ローディングアニメーション
+  const [loading, setLoading] = useState(0)
+  useEffect(() => {
+    const id = setInterval(() => {
+      setLoading((t) => (t < 100 ? t + 1 : t))
+    }, 25)
+    return () => clearInterval(id)
+  }, [])
+
+  // リッキーくんアニメーション
   const [over, setOver] = useState(false)
   const changeBeforeRicky = () => {
     setTimeout(() => {
@@ -18,14 +28,6 @@ const Home: NextPage = () => {
       setOver(true)
     }, 500)
   }
-
-  const [loading, setLoading] = useState(0)
-  useEffect(() => {
-    const id = setInterval(() => {
-      setLoading((t) => (t < 100 ? t + 1 : t))
-    }, 25)
-    return () => clearInterval(id)
-  }, [])
 
   return (
     <>
@@ -44,12 +46,12 @@ const Home: NextPage = () => {
               <ReactFullpage.Wrapper>
                 <div className="section bg-white">
                   <div className="container mx-auto h-screen w-screen">
-                    <div className="max-w-[33vw] py-10">
+                    <div className="p-4 md:max-w-[33vw] md:py-10 md:px-0">
                       <Image src="/images/logo.png" alt="サイト名" width={554.79} height={130.27} />
                     </div>
-                    <div className="flex items-center justify-center">
+                    <div className="my-[15vh] flex items-center justify-center md:my-0">
                       <div
-                        className="h-[50vh] w-auto shrink transition-all duration-1000 hover:rotate-y-360"
+                        className="h-[40vh] w-auto shrink transition-all duration-1000 hover:rotate-y-360 md:h-[50vh]"
                         onMouseOver={changeAfterRicky}
                         onMouseOut={changeBeforeRicky}
                       >
