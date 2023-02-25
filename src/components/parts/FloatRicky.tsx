caa5251f5a8eb5807e6d83af5c49044e24f9b889
anyushu <anyushu0526@gmail.com>
2023-02-25 15:37:15 +0900

    feat: リッキーくんのセリフ追加

--- src/components/parts/FloatRicky.tsx
+++ src/components/parts/FloatRicky.tsx
@@ -1,3 +1,10 @@
+const words = [
+  '',
+  'レーススタート！',
+  '世界でも例のない\nユニークな競馬！',
+  '最初の関門\n第一障害！',
+]
+
 const FloatRicky = ({ activeSection }: { activeSection: number }) => {
   return (
     <>
@@ -8,15 +15,17 @@ const FloatRicky = ({ activeSection }: { activeSection: number }) => {
         } fixed top-0 right-0 z-10 h-60 w-36 bg-[url('/images/ricky_float.png')] bg-contain bg-no-repeat transition-all duration-500`}
       ></div>
       {/* リッキーくん吹き出し */}
-      <div
-        className={`${
-          activeSection > 0 ? 'opacity-100' : 'opacity-0'
-        } fixed top-16 right-36 z-10 h-[115px] w-[305px] bg-[url('/images/ricky_float_words.png')] bg-contain bg-no-repeat transition-all duration-500`}
-      >
-        <div className="flex items-center justify-start p-10">
-          <p className="text-2xl font-bold">レーススタート！</p>
+      {activeSection in words && (
+        <div
+          className={`${
+            activeSection > 0 ? 'opacity-100' : 'opacity-0'
+          } fixed top-16 right-36 z-10 flex h-[115px] w-[305px] items-center justify-start bg-[url('/images/ricky_float_words.png')] bg-contain bg-no-repeat transition-all duration-500`}
+        >
+          <div className="px-10">
+            <p className="whitespace-pre-wrap text-2xl font-bold">{words[activeSection]}</p>
+          </div>
         </div>
-      </div>
+      )}
     </>
   )
 }
