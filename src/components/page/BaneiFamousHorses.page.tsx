3f03d30c6997b24788e503379639f6538a723106
anyushu <anyushu0526@gmail.com>
2023-02-26 13:29:06 +0900

    feat(PC): 「ばんえい」が誇る名馬たちのモーダル追加

--- src/components/page/BaneiFamousHorses.page.tsx
+++ src/components/page/BaneiFamousHorses.page.tsx
@@ -1,7 +1,26 @@
 import Image from 'next/image'
+import { useState } from 'react'
+import Modal from 'react-modal'
 import Slider from 'react-slick'
 
 const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
+  const [slider1ModalIsOpen, setSlider1ModalIsOpenOpen] = useState(false)
+  const [slider2ModalIsOpen, setSlider2ModalIsOpenOpen] = useState(false)
+  const [slider3ModalIsOpen, setSlider3ModalIsOpenOpen] = useState(false)
+  const [slider4ModalIsOpen, setSlider4ModalIsOpenOpen] = useState(false)
+  const [slider5ModalIsOpen, setSlider5ModalIsOpenOpen] = useState(false)
+  const [slider6ModalIsOpen, setSlider6ModalIsOpenOpen] = useState(false)
+
+  const modalStyle = {
+    overlay: {
+      zIndex: 99999,
+    },
+    content: {
+      border: 'solid 5px #00B17D',
+      borderRadius: 0,
+    },
+  }
+
   return (
     <div className="section bg-[url('/images/bg_1.png')] bg-cover bg-no-repeat">
       <div
@@ -46,15 +65,186 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             centerMode={true}
             centerPadding="100px"
           >
-            <Image src="/images/famous_horses_1.png" width={640} height={480} alt="名馬" />
-            <Image src="/images/famous_horses_2.png" width={640} height={480} alt="名馬" />
-            <Image src="/images/famous_horses_3.png" width={640} height={480} alt="名馬" />
-            <Image src="/images/famous_horses_4.png" width={640} height={480} alt="名馬" />
-            <Image src="/images/famous_horses_5.png" width={640} height={480} alt="名馬" />
-            <Image src="/images/famous_horses_6.png" width={640} height={480} alt="名馬" />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider1ModalIsOpenOpen(true)}
+              src="/images/famous_horses_1.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider2ModalIsOpenOpen(true)}
+              src="/images/famous_horses_2.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider3ModalIsOpenOpen(true)}
+              src="/images/famous_horses_3.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider4ModalIsOpenOpen(true)}
+              src="/images/famous_horses_4.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider5ModalIsOpenOpen(true)}
+              src="/images/famous_horses_5.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
+            <Image
+              className="cursor-pointer"
+              onClick={() => setSlider6ModalIsOpenOpen(true)}
+              src="/images/famous_horses_6.png"
+              width={640}
+              height={480}
+              alt="名馬"
+            />
           </Slider>
         </div>
       </div>
+
+      <Modal
+        isOpen={slider1ModalIsOpen}
+        onRequestClose={() => setSlider1ModalIsOpenOpen(false)}
+        style={{
+          overlay: {
+            zIndex: 99999,
+          },
+          content: {
+            border: 'solid 5px #00B17D',
+            borderRadius: 0,
+          },
+        }}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider1ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_1.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">イレネー</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={slider2ModalIsOpen}
+        onRequestClose={() => setSlider2ModalIsOpenOpen(false)}
+        style={modalStyle}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider2ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_2.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">カネサブラック</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={slider3ModalIsOpen}
+        onRequestClose={() => setSlider3ModalIsOpenOpen(false)}
+        style={modalStyle}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider3ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_3.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">キンタロー</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={slider4ModalIsOpen}
+        onRequestClose={() => setSlider4ModalIsOpenOpen(false)}
+        style={modalStyle}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider4ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_4.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">スーパーペガサス</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={slider5ModalIsOpen}
+        onRequestClose={() => setSlider5ModalIsOpenOpen(false)}
+        style={modalStyle}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider5ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_5.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">ホクショウマサル</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        isOpen={slider6ModalIsOpen}
+        onRequestClose={() => setSlider6ModalIsOpenOpen(false)}
+        style={modalStyle}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setSlider6ModalIsOpenOpen(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div className="flex h-full items-center px-11">
+          <Image src="/images/famous_horses_6.png" width={640} height={480} alt="名馬" />
+          <div className="ml-12">
+            <h3 className="mb-3 text-2xl">マルセンバージ</h3>
+            <p>テキストが入りますテキストが入ります</p>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
