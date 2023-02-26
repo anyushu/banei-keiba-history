1a234fb916f8a89324d22020ab7dc30492f8e41e
anyushu <anyushu0526@gmail.com>
2023-02-26 14:30:58 +0900

    feat(SP): スタートページ

--- src/components/page/Start.page.tsx
+++ src/components/page/Start.page.tsx
@@ -17,8 +17,36 @@ const Start = ({ fullpageApi }: { fullpageApi: fullpageApi }) => {
   return (
     <div id="startPage" className="section bg-white">
       <div className="container z-10 mx-auto h-screen w-screen px-4 md:px-0">
-        <div className="py-4 md:max-w-[33vw] md:py-10">
-          <Image src="/images/logo.png" alt="サイト名" width={554.79} height={130.27} />
+        <div className="flex max-w-full items-center justify-start py-4 md:max-w-[33vw] md:py-10">
+          <a
+            className="mr-3 block"
+            href="https://banei-keiba.or.jp/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="/images/header_logo_tokachi.svg"
+              alt="ばんえい十勝"
+              width={102.63}
+              height={130.27}
+            />
+          </a>
+          <div className="mr-6">
+            <Image
+              src="/images/header_logo_hokkaido.svg"
+              alt="北海道遺産"
+              width={91.37}
+              height={130.27}
+            />
+          </div>
+          <div>
+            <Image
+              src="/images/header_logo_banei.svg"
+              alt="ばんえい競馬場"
+              width={441.61}
+              height={130.27}
+            />
+          </div>
         </div>
         <div className="my-[15vh] flex items-center justify-center md:my-0">
           <div
