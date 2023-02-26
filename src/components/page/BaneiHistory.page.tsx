f4c053841b2753e15fa000cb833f84a838dcf624
anyushu <anyushu0526@gmail.com>
2023-02-26 15:49:29 +0900

    fix(SP): 歩みのレスポンシブ化

--- src/components/page/BaneiHistory.page.tsx
+++ src/components/page/BaneiHistory.page.tsx
@@ -24,18 +24,20 @@ const BaneiHistory = ({ activeSection }: { activeSection: number }) => {
             <span className="block text-xs md:text-lg">History of Banei</span>
           </span>
         </h2>
-        <div className="max-w-[850px] py-4">
+        <div className="max-w-[290px] py-4 md:max-w-[850px]">
           <div className="flex items-start justify-center">
             <Image src="/images/hidume.png" width={600} height={600} alt="ばんえいの歩み" />
           </div>
           <div className="mt-4 border border-gray-500 bg-white p-4">
-            <h3 className="mb-4 text-center text-2xl font-bold">ルーツである「お祭りばん馬」</h3>
-            <p>
+            <h3 className="mb-4 text-center text-base font-bold md:text-2xl">
+              ルーツである「お祭りばん馬」
+            </h3>
+            <p className="text-xs md:text-base">
               農村ではばん馬の力を競わせる娯楽として「お祭りばん馬」が誕生しました。これがばんえい競馬のルーツです。
               <br />
               「そりに乗って馬を操る」というスタイルは、当時の外国人指導者による「馬を一人で操れる技術」が元と言われています。
             </p>
-            <p className="mt-3 text-right">写真/長澤和恵</p>
+            <p className="mt-3 text-right text-xs md:text-base">写真/長澤和恵</p>
           </div>
         </div>
       </div>
