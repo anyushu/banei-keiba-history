75e81266d272d9ac5fd8a1aa3c1f383de10442d5
anyushu <anyushu0526@gmail.com>
2023-02-27 20:24:31 +0900

    fix(txt): コンテンツ差し替え

--- src/components/page/BaneiFamousHorses.page.tsx
+++ src/components/page/BaneiFamousHorses.page.tsx
@@ -162,14 +162,17 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
         <div className="flex h-full flex-col items-center md:flex-row md:px-11">
           <Image
             src="/images/famous_horses_1.png"
-            className="mt-auto md:mt-0"
+            className="mt-auto shrink md:mt-0"
             width={640}
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">イレネー</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">年代：1908年～1928年</p>
+            <p className="text-sm md:text-base">
+              1910年フランスから十勝種馬牧場（現：家畜改良センター十勝牧場）の導入されたペルシュロン種。18年間に1,074頭の種雌馬と配合を行い、579頭の産駒を生産、その子孫はさらに多くの優良馬を生み出しました。「偉大なる種牡馬」として帯広競馬場入口にイレネー像が奉られ、その年デビューした若駒が競う重賞レース「イレネー記念」が3月に開催されています。
+            </p>
           </div>
         </div>
       </Modal>
@@ -193,9 +196,12 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">カネサブラック</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">現役期間：2004年～2013年</p>
+            <p className="text-sm md:text-base">
+              「ばんえい記念」を3勝、4市重賞「帯広記念」「旭川記念」「岩見沢記念」「北見記念」を全制覇。2019年にオレノココロが更新しますが、重賞最多21勝記録を達成。186戦72勝。
+            </p>
           </div>
         </div>
       </Modal>
@@ -219,9 +225,12 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">キンタロー</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">現役期間：1979年～1986年</p>
+            <p className="text-sm md:text-base">
+              ばんえい史上初の賞金1億円馬。「農林水産大臣賞典」（現：ばんえい記念）を3度優勝するなど、スターホースとして当時の競馬場に活気をもたらし、現在も語り継がれる名馬です。102戦32勝。
+            </p>
           </div>
         </div>
       </Modal>
@@ -245,9 +254,12 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">スーパーペガサス</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">現役期間：1998年～2007年</p>
+            <p className="text-sm md:text-base">
+              2003年から2006年にかけて「ばんえい記念」を4連覇。「ばんえい記念」4勝は現在もこの馬1頭のみ。155戦42勝中、重賞は20勝と華々しい結果を残しました。
+            </p>
           </div>
         </div>
       </Modal>
@@ -271,9 +283,12 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">ホクショウマサル</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">現役期間：2013年～2021年</p>
+            <p className="text-sm md:text-base">
+              2歳「イレネー記念」、3歳「ばんえいダービー」の世代王者になるも、病気が悪化し長期休養。病を乗り越えた復帰後に、地方競馬公式最多連勝記録となる31連勝を達成。そして「ばんえい記念」2度目の挑戦で優勝しました。その後病に倒れ、まさに波乱万丈な生涯となりました。125戦48勝。
+            </p>
           </div>
         </div>
       </Modal>
@@ -297,9 +312,12 @@ const BaneiFamousHorses = ({ activeSection }: { activeSection: number }) => {
             height={480}
             alt="名馬"
           />
-          <div className="mt-5 mb-auto md:my-0 md:ml-12">
+          <div className="mt-5 mb-auto max-w-md md:my-0 md:ml-12">
             <h3 className="mb-3 text-lg md:text-2xl">マルセンバージ</h3>
-            <p className="text-sm md:text-base">テキストが入りますテキストが入ります</p>
+            <p className="my-3 text-xs md:text-sm">現役期間：1988年～1995年</p>
+            <p className="text-sm md:text-base">
+              ベルジャン種の純血。純血種で「ばんえい記念」を優勝したのはこの馬のみ。農林水産大臣賞典（現：ばんえい記念）を2度優勝するなど、139戦27勝を挙げました。
+            </p>
           </div>
         </div>
       </Modal>
