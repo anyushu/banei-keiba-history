a5d86db30574ac9cdf1ec7396381e824dc34fbef
anyushu <anyushu0526@gmail.com>
2023-02-27 15:26:59 +0900

    onorientationchange修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,22 +5,25 @@ import 'swiper/css/effect-fade'
 
 import type { AppProps } from 'next/app'
 import { DefaultSeo } from 'next-seo'
+import { useEffect } from 'react'
 
 import { defaultSeo } from '@/utils/next-seo.config'
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  window.onorientationchange = function () {
-    switch (window.orientation) {
-      case 0:
-        break
-      case 90:
-        alert('画面を縦にしてください')
-        break
-      case -90:
-        alert('画面を縦にしてください')
-        break
+  useEffect(() => {
+    window.onorientationchange = function () {
+      switch (window.orientation) {
+        case 0:
+          break
+        case 90:
+          alert('画面を縦にしてください')
+          break
+        case -90:
+          alert('画面を縦にしてください')
+          break
+      }
     }
-  }
+  }, [])
   return (
     <>
       <DefaultSeo {...defaultSeo} />
