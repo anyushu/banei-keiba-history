bfa4e0efa3c61dcd921ee8c8f4cc4bdb870ae424
anyushu <anyushu0526@gmail.com>
2023-03-06 14:47:42 +0900

    fix(style): float_scroll のいち調節

--- src/components/parts/FloatScrollIcon.tsx
+++ src/components/parts/FloatScrollIcon.tsx
@@ -5,10 +5,10 @@ const FloatScrollIcon = ({ activeSection }: { activeSection: number }) => {
     <div
       className={`${
         activeSection == 0 || activeSection == 12 ? 'opacity-0' : 'opacity-100'
-      } fixed top-2 -right-2 left-auto z-10 hidden transition-all duration-500 lg:top-4 lg:right-auto lg:left-4 lg:block`}
+      } fixed top-2 -right-2 left-auto z-10 hidden transition-all duration-500 lg:top-1/2 lg:right-0 lg:block lg:translate-y-[-180px]`}
     >
       <Image
-        className="h-[60px] lg:h-auto"
+        className="h-[60px] lg:h-[60px]"
         src="/images/float_scroll.svg"
         width={55.89}
         height={120}
