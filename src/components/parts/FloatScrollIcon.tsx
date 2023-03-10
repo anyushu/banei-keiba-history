f4200c7d762a5282f00de198ed6d8d768d32bafd
anyushu <anyushu0526@gmail.com>
2023-03-10 11:07:36 +0900

    fix: スマホのドットメニューの上部に、PCと同様に「SCROLL」表示を挿入してください

--- src/components/parts/FloatScrollIcon.tsx
+++ src/components/parts/FloatScrollIcon.tsx
@@ -5,10 +5,10 @@ const FloatScrollIcon = ({ activeSection }: { activeSection: number }) => {
     <div
       className={`${
         activeSection == 0 || activeSection == 12 ? 'opacity-0' : 'opacity-100'
-      } fixed top-2 -right-2 left-auto z-10 hidden transition-all duration-500 lg:top-1/2 lg:right-0 lg:block lg:translate-y-[-180px]`}
+      } fixed right-0 left-auto top-1/2 z-10 translate-y-[-160px] transition-all duration-500 lg:right-0 lg:translate-y-[-180px]`}
     >
       <Image
-        className="h-[60px] lg:h-[60px]"
+        className="h-[40px] lg:h-[60px]"
         src="/images/float_scroll.svg"
         width={55.89}
         height={120}
