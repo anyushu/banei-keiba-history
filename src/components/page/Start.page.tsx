b67bfaf84413e95e165bdedfe69f02e817f1f011
anyushu <anyushu0526@gmail.com>
2023-03-11 21:56:14 +0900

    fix: ページの操作説明をローディング画面後に表示

--- src/components/page/Start.page.tsx
+++ src/components/page/Start.page.tsx
@@ -1,9 +1,12 @@
 import { fullpageApi } from '@fullpage/react-fullpage'
 import Image from 'next/image'
 import { useState } from 'react'
+import Modal from 'react-modal'
+import Slider from 'react-slick'
 
 const Start = ({ fullpageApi }: { fullpageApi: fullpageApi }) => {
   const [over, setOver] = useState(false)
+  const [introModal, setIntroModal] = useState(true)
   const changeBeforeRicky = () => {
     setTimeout(() => {
       setOver(false)
@@ -14,6 +17,16 @@ const Start = ({ fullpageApi }: { fullpageApi: fullpageApi }) => {
       setOver(true)
     }, 500)
   }
+  const modalStyle = {
+    overlay: {
+      zIndex: 40,
+    },
+    content: {
+      border: 'solid 5px #00B17D',
+      borderRadius: 0,
+    },
+  }
+
   return (
     <div id="startPage" className="section bg-white">
       <div className="container z-10 mx-auto px-4 lg:px-0">
@@ -76,6 +89,46 @@ const Start = ({ fullpageApi }: { fullpageApi: fullpageApi }) => {
           </button>
         </div>
       </div>
+      <Modal
+        isOpen={introModal}
+        onRequestClose={() => setIntroModal(false)}
+        style={modalStyle}
+        ariaHideApp={false}
+      >
+        <button
+          className=" absolute top-0 right-0"
+          type="button"
+          onClick={() => setIntroModal(false)}
+        >
+          <Image src="/images/icon_close.svg" width={52} height={52} alt="close" />
+        </button>
+        <div id="introSlide" className="pt-9 lg:px-12 lg:pt-0">
+          <Slider
+            className="hidden lg:block"
+            dots={true}
+            infinite={true}
+            speed={500}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            <Image src="/images/intro_pc_1.jpg" width={1232} height={622} alt="はじめに" />
+            <Image src="/images/intro_pc_2.jpg" width={1232} height={622} alt="はじめに" />
+            <Image src="/images/intro_pc_3.jpg" width={1232} height={622} alt="はじめに" />
+          </Slider>
+          <Slider
+            className="block lg:hidden"
+            dots={true}
+            infinite={true}
+            speed={500}
+            slidesToShow={1}
+            slidesToScroll={1}
+          >
+            <Image src="/images/intro_sp_1.jpg" width={449} height={791} alt="はじめに" />
+            <Image src="/images/intro_sp_2.jpg" width={449} height={791} alt="はじめに" />
+            <Image src="/images/intro_sp_3.jpg" width={449} height={791} alt="はじめに" />
+          </Slider>
+        </div>
+      </Modal>
     </div>
   )
 }
